621e073af8ff3a2b7e307bf76afd315f776f73c6
Isam Hakmi <isam.hakmi@brightstraining.com>
2023-02-16 14:13:42 +0100

    changes saved

--- GitCheckpoint1/Program.cs
+++ GitCheckpoint1/Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("This is the Git checkpoint task 1.1");
+            CheckTest.testMessage("Task 2 checked");
         }
     }
 }
